2d024ea258a4abaab3d4d915760fa998e6a939e1
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-10-09 09:46:20 -0500

    add functionality for remoting with a specified credential

--- SCOrchDev-SQL.psm1
+++ SCOrchDev-SQL.psm1
@@ -18,6 +18,17 @@
     .Parameter timeout
         timeout property for SQL query. Default is 60 seconds
 
+    .Parameter Credential
+        if specified the credential will be used to connect to an SMA box and launch
+        the SQL query as the target user.
+
+        If specified the computer parameter becomes optional. This is the computer to connect
+        to with this credential (using CredSSP authentication)
+
+    .Parameter Computer
+         This is the computer to connect to if Credntial is specified (using CredSSP authentication)
+         Defaults to localhost
+
     .Example
         # run a simple query
 
@@ -25,69 +36,140 @@
         $parameters = @{}
         Invoke-SqlQuery -query "SELECT GroupID, GroupName From [dbo].[Group] WHERE GroupName=@GroupName" -parameters @{"@GroupName"="genmills\groupName"} -connectionString $connectionString;
         Invoke-SqlQuery -query "SELECT GroupID, GroupName From [dbo].[Group]" -parameters @{} -connectionString $connectionString;
-   
 #>
 function Invoke-SqlQuery
 {
     Param(
-        [Parameter(Mandatory = $True)]
+        [Parameter(
+                Mandatory = $True
+        )]
         [string]
-        $query,
+        $Query, 
         
-        [Parameter(Mandatory = $False)]
+        [Parameter(
+                Mandatory = $False
+        )]
         [System.Collections.Hashtable]
-        $parameters,
-        
-        [Parameter(Mandatory = $True)]
-        [string]
-        $connectionString, 
-        
-        [Parameter(Mandatory = $False)]
-        [int]
-        $timeout = 60
-    )
-    # convert parameter string to array of SqlParameters
-    try
-    {
-        Write-Debug -Message "`$query [$query]"
-        Write-Debug -Message "`$connectionString [$connectionString]"
-        Write-Debug -Message "`$timeout [$timeout]"
-        foreach($paramKey in $parameters.Keys)
-        {
-            Write-Debug -Message "`$paramKey   [$paramKey]"
-            Write-Debug -Message "`$ParamValue [$($parameters[$paramKey])]"
-        }
-        $sqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection -ArgumentList $connectionString
-        $sqlConnection.Open()
+        $Parameters, 
 
-        #Create a command object
-        $sqlCommand = $sqlConnection.CreateCommand()
-        $sqlCommand.CommandText = $query
-        if($parameters)
-        {
-            foreach($key in $parameters.Keys)
+        [Parameter(
+                Mandatory = $True
+        )]
+        [string]
+        $ConnectionString, 
+
+        [Parameter(
+                Mandatory = $False
+        )]
+        [int]
+        $Timeout = 60,
+
+        [Parameter(
+                Mandatory = $False,
+                ParameterSetName = 'Credential'
+        )]
+        [pscredential]
+        $Credential,
+
+        [Parameter(
+                Mandatory = $False,
+                ParameterSetName = 'Credential'
+        )]
+        [pscredential]
+        $Computer = 'localhost'
+    )
+    
+    Write-Debug -Message "`$query [$query]"
+    Write-Debug -Message "`$connectionString [$connectionString]"
+    Write-Debug -Message "`$timeout [$timeout]"
+    $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+    if($Credential)
+    {
+        $Result = Invoke-Command -ComputerName $Computer -Credential $Credential -Authentication Credssp -ScriptBlock {
+            $parameters = $Using:Parameters
+            $connectionString = $Using:connectionString
+            $query = $Using:query
+            $timeout = $Using:timeout
+            try
             {
-                $null = $sqlCommand.Parameters.AddWithValue($key, $parameters[$key])
+                foreach($paramKey in $parameters.Keys)
+                {
+                    Write-Debug -Message "`$paramKey   [$paramKey]"
+                    Write-Debug -Message "`$ParamValue [$($parameters[$paramKey])]"
+                }
+                $sqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection `
+                                            -ArgumentList $connectionString
+                $sqlConnection.Open()
+
+                #Create a command object
+                $sqlCommand = $sqlConnection.CreateCommand()
+                $sqlCommand.CommandText = $query
+                if($parameters)
+                {
+                    foreach($key in $parameters.Keys)
+                    {
+                        $null = $sqlCommand.Parameters.AddWithValue($key, $parameters[$key])
+                    }
+                }
+                $sqlCommand.CommandTimeout = $timeout
+
+                #Execute the Command
+                $sqlReader = $sqlCommand.ExecuteReader()
+
+                $Datatable = New-Object -TypeName System.Data.DataTable
+                $Datatable.Load($sqlReader)
+            }
+            Finally
+            {
+                if($sqlConnection -and $sqlConnection.State -ne [System.Data.ConnectionState]::Closed)
+                {
+                    $sqlConnection.Close()
+                }
+            }
+            return $Datatable -as [System.Data.DataTable]
+        }
+        $Result
+    }
+    else
+    {
+        try
+        {
+            foreach($paramKey in $parameters.Keys)
+            {
+                Write-Debug -Message "`$paramKey   [$paramKey]"
+                Write-Debug -Message "`$ParamValue [$($parameters[$paramKey])]"
+            }
+            $sqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection `
+                                        -ArgumentList $connectionString
+            $sqlConnection.Open()
+
+            #Create a command object
+            $sqlCommand = $sqlConnection.CreateCommand()
+            $sqlCommand.CommandText = $query
+            if($parameters)
+            {
+                foreach($key in $parameters.Keys)
+                {
+                    $null = $sqlCommand.Parameters.AddWithValue($key, $parameters[$key])
+                }
+            }
+
+            $sqlCommand.CommandTimeout = $timeout
+
+            #Execute the Command
+            $sqlReader = $sqlCommand.ExecuteReader()
+
+            $Datatable = New-Object -TypeName System.Data.DataTable
+            $Datatable.Load($sqlReader)
+        }
+        Finally
+        {
+            if($sqlConnection -and $sqlConnection.State -ne [System.Data.ConnectionState]::Closed)
+            {
+                $sqlConnection.Close()
             }
         }
-
-        $sqlCommand.CommandTimeout = $timeout
-
-        #Execute the Command
-        $sqlReader = $sqlCommand.ExecuteReader()
-
-        $Datatable = New-Object -TypeName System.Data.DataTable
-        $Datatable.Load($sqlReader)
-
-
         return $Datatable
-    }
-    finally
-    {
-        if($sqlConnection -and $sqlConnection.State -ne [System.Data.ConnectionState]::Closed)
-        {
-            $sqlConnection.Close()
-        }
     }
 }
 Export-ModuleMember -Function * -Verbose:$False
